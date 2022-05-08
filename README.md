c1e6ceb1079ab3fc5284b28e26525b31584d018e
PerryLets <heitorpolycarpomacedo@gmail.com>
2022-05-08 02:52:59 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Perry's Fedora system setup
+
+Simple script for when I update to Fedora 36. I know you can just upgrade the distro, but I want to change the drive where it's installed.
---
+++ setup.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/bash
+  sudo dnf -y check-update; sudo dnf -y upgrade
+  sudo dnf -y install dnf -y-plugins-core
+  # RPM Fusion Free
+  sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+  # RPM Fusion Non-Free
+  sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+  # RPM Fusion appstream
+  sudo dnf -y groupupdate core
+  # Vs Code repo
+  sudo dnf -y config-manager --add-repo https://packages.microsoft.com/keys/microsoft.asc
+  sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+  #Unityhub repo
+  sudo sh -c 'echo -e "[unityhub]\nname=Unity Hub\nbaseurl=https://hub.unity3d.com/linux/repos/rpm/stable\nenabled=1\ngpgcheck=1\ngpgkey=https://hub.unity3d.com/linux/repos/rpm/stable/repodata/repomd.xml.key\nrepo_gpgcheck=1" > /etc/yum.repos.d/unityhub.repo'
+  #Docker repo
+  sudo dnf -y remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine docker-engine
+  sudo dnf -y config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+  #Flathub repo
+  sudo dnf install -y flatpak
+  sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+  #Fedora repo to be safe
+  sudo flatpak remote-add --if-not-exists fedora oci+https://registry.fedoraproject.org
+
+  sudo snf -y check-update; sudo dnf -y upgradeP
+#Packages and git repos
+  #TODO: Install:
+    #! - rtl8822bu wireless driver (maybe not needed)
+    # - node.js
+    # - dotnet sdk
+    # - nuget
+    # - python3
+    # - gcc
+    # - g++
+    # - code
+    # - git (just to be sure)
+    # - unityhub
+    # - docker
+    # - fira code
+    # - MesloLGS NF
+    # - audacity
+    # - krita
+    # - vlc
+    # - discord
+    # - spotify
+    # - chrome
+    # - tilix
+    # - zsh
+    # - zsh-syntax-highlighting
+    # - zsh-autosuggestions
+    # - vim
+    # - irssi
+    # - crontab
+  #TODO: Remove:
+    # - firefox
+    #TODO: Add more apps to be removed (if needed)
