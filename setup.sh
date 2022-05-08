5477a3da72aac0a057770b7a5fdbc8d6e6390245
Perry <62119804+PerryLets@users.noreply.github.com>
2022-05-08 02:56:38 -0300

    Fix typos

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
   sudo dnf -y check-update; sudo dnf -y upgrade
-  sudo dnf -y install dnf -y-plugins-core
+  sudo dnf -y install dnf-plugins-core
   # RPM Fusion Free
   sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
   # RPM Fusion Non-Free
   sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
   # RPM Fusion appstream
-  sudo dnf -y groupupdate core
+  sudo dnf -y group update core
   # Vs Code repo
   sudo dnf -y config-manager --add-repo https://packages.microsoft.com/keys/microsoft.asc
   sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
@@ -21,7 +21,7 @@
   #Fedora repo to be safe
   sudo flatpak remote-add --if-not-exists fedora oci+https://registry.fedoraproject.org
 
-  sudo snf -y check-update; sudo dnf -y upgradeP
+  sudo dnf -y check-update; sudo dnf -y upgrade
 #Packages and git repos
   #TODO: Install:
     #! - rtl8822bu wireless driver (maybe not needed)
