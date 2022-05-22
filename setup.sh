a3ce3e3bdb50ea254f095ac7ca62f4db88879df7
PerryLets <heitorpolycarpomacedo@gmail.com>
2022-05-21 21:35:39 -0300

    Remove unityhub

--- setup.sh
+++ setup.sh
@@ -11,8 +11,6 @@
     # Vs Code repo
       sudo dnf -y config-manager --add-repo https://packages.microsoft.com/keys/microsoft.asc
       sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-    #Unityhub repo
-      sudo sh -c 'echo -e "[unityhub]\nname=Unity Hub\nbaseurl=https://hub.unity3d.com/linux/repos/rpm/stable\nenabled=1\ngpgcheck=1\ngpgkey=https://hub.unity3d.com/linux/repos/rpm/stable/repodata/repomd.xml.key\nrepo_gpgcheck=1" > /etc/yum.repos.d/unityhub.repo'
     #Docker repo
       sudo dnf -y remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine docker-engine
       sudo dnf -y config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
@@ -34,7 +32,6 @@
       # - g++
       # - code
       # - git (just to be sure)
-      # - unityhub
       # - docker
       # - fira code
       # - MesloLGS NF
@@ -52,5 +49,4 @@
       # - irssi
       # - crontab (I had to install it on Fedora 35 for some reason)
     #TODO: Remove:
-      # - firefox
       #TODO: Add more apps to be removed (if needed)
